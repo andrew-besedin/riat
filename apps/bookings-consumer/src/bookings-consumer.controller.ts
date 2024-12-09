f3ee62557e4532594463bfacbb7082af8b580d62
andrewprog97 <andrewprog97@gmail.com>
2024-12-09 23:15:34 +0300

    add nodemailer

--- apps/bookings-consumer/src/bookings-consumer.controller.ts
+++ apps/bookings-consumer/src/bookings-consumer.controller.ts
@@ -1,10 +1,28 @@
 import { Controller, Logger } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+import * as nodemailer from 'nodemailer';
 
 @Controller()
 export class BookingsConsumerController {
   @EventPattern('booked')
-  onBooked() {
+  async onBooked() {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'keshawn.schaefer80@ethereal.email',
+        pass: 'cs5PqztZXzS48K45hY',
+      },
+    });
+
+    await transporter.verify();
+
+    transporter.sendMail({
+      from: 'keshawn.schaefer80@ethereal.email',
+      to: 'andrewprog97@gmail.com',
+      subject: 'Booked tickets',
+      html: '<b>Hello!!!</b> here are your tickets',
+    });
     Logger.log('EMAIL SENT!!!');
   }
 }
